08ee3a47ffe1d22c918a5ab69f1beb1694dc04ed
Isaac Grove <goodgrove@gmail.com>
2025-11-28 17:34:32 +1100

    Working. Want to add slider and zoom functionality

--- script.js
+++ script.js
@@ -15,32 +15,252 @@ const COLOR_LAND = 0xffffff;
 
 const portfolioData = [
     {
-        name: "Tokyo, Japan",
-        lat: 35.6762,
-        lon: 139.6503,
-        preview: "https://picsum.photos/id/1015/200/150",
-        images: ["https://picsum.photos/id/1015/800/600", "https://picsum.photos/id/1016/800/600", "https://picsum.photos/id/1018/800/600"]
+        name: "Amsterdam, Netherlands",
+        lat: 52.3676,
+        lon: 4.9041,
+        preview: "images/Amsterdam/Amsterdam_1.jpg",
+        images: [
+            "images/Amsterdam/Amsterdam_1.jpg",
+            "images/Amsterdam/Amsterdam_2.jpg",
+            "images/Amsterdam/Amsterdam_3.jpg",
+            "images/Amsterdam/Amsterdam_4.jpg"
+        ]
     },
     {
-        name: "Reykjavik, Iceland",
-        lat: 64.1466,
-        lon: -21.9426,
-        preview: "https://picsum.photos/id/1039/200/150",
-        images: ["https://picsum.photos/id/1039/800/600", "https://picsum.photos/id/1040/800/600", "https://picsum.photos/id/1043/800/600"]
+        name: "Banff, Canada",
+        lat: 51.1784,
+        lon: -115.5708,
+        preview: "images/Banff/Banff_1.jpg",
+        images: [
+            "images/Banff/Banff_1.jpg",
+            "images/Banff/Banff_2.jpg",
+            "images/Banff/Banff_3.jpg",
+            "images/Banff/Banff_4.jpg",
+            "images/Banff/Banff_5.jpg",
+            "images/Banff/Banff_6.jpg",
+            "images/Banff/Banff_7.jpg",
+            "images/Banff/Banff_8.jpg"
+        ]
+    },
+    {
+        name: "Barcelona, Spain",
+        lat: 41.3902,
+        lon: 2.1540,
+        preview: "images/Barcelona/Barcelona_1.jpg",
+        images: [
+            "images/Barcelona/Barcelona_1.jpg",
+            "images/Barcelona/Barcelona_2.jpg",
+            "images/Barcelona/Barcelona_3.jpg"
+        ]
+    },
+    {
+        name: "Bellingen, Australia",
+        lat: -30.45224,
+        lon: 152.89796,
+        preview: "images/Bellingen/Bellingen_1.jpg",
+        images: [
+            "images/Bellingen/Bellingen_1.jpg",
+            "images/Bellingen/Bellingen_2.jpg",
+            "images/Bellingen/Bellingen_3.jpg",
+            "images/Bellingen/Bellingen_4.jpg",
+            "images/Bellingen/Bellingen_5.jpg",
+            "images/Bellingen/Bellingen_6.jpg"
+        ]
+    },
+    {
+        name: "Byron Bay, Australia",
+        lat: -28.64306,
+        lon: 153.61500,
+        preview: "images/Byron_Bay/Byron_Bay_1.jpg",
+        images: [
+            "images/Byron_Bay/Byron_Bay_1.jpg",
+            "images/Byron_Bay/Byron_Bay_2.jpg",
+            "images/Byron_Bay/Byron_Bay_3.jpg",
+            "images/Byron_Bay/Byron_Bay_4.jpg"
+        ]
+    },
+    {
+        name: "Chiang Mai, Thailand",
+        lat: 18.7953,
+        lon: 98.9986,
+        preview: "",
+        images: []
+    },
+    {
+        name: "Crescent Head, Australia",
+        lat: -31.1887,
+        lon: 152.9730,
+        preview: "images/Crescent_Head/Crescent_Head_1.jpg",
+        images: [
+            "images/Crescent_Head/Crescent_Head_1.jpg",
+            "images/Crescent_Head/Crescent_Head_2.jpg",
+            "images/Crescent_Head/Crescent_Head_3.jpg",
+            "images/Crescent_Head/Crescent_Head_4.jpg",
+            "images/Crescent_Head/Crescent_Head_5.jpg"
+        ]
+    },
+    {
+        name: "Dubrovnik, Croatia",
+        lat: 42.6507,
+        lon: 18.0944,
+        preview: "images/Dubrovnik/Dubrovnik_1.jpg",
+        images: [
+            "images/Dubrovnik/Dubrovnik_1.jpg",
+            "images/Dubrovnik/Dubrovnik_2.jpg",
+            "images/Dubrovnik/Dubrovnik_3.jpg",
+            "images/Dubrovnik/Dubrovnik_4.jpg"
+        ]
+    },
+    {
+        name: "Florence, Italy",
+        lat: 43.7696,
+        lon: 11.2558,
+        preview: "images/Florence/Florence_1.jpg",
+        images: [
+            "images/Florence/Florence_1.jpg",
+            "images/Florence/Florence_2.jpg",
+            "images/Florence/Florence_3.jpg",
+            "images/Florence/Florence_4.jpg"
+        ]
+    },
+    {
+        name: "Holbox, Mexico",
+        lat: 21.5136,
+        lon: -87.2716,
+        preview: "images/Holbox/Holbox_1.jpg",
+        images: [
+            "images/Holbox/Holbox_1.jpg",
+            "images/Holbox/Holbox_2.jpg",
+            "images/Holbox/Holbox_3.jpg"
+        ]
+    },
+    {
+        name: "Jerusalem, Israel",
+        lat: 31.7683,
+        lon: 35.2137,
+        preview: "images/Jerusalem/Jerusalem_1.jpg",
+        images: [
+            "images/Jerusalem/Jerusalem_1.jpg",
+            "images/Jerusalem/Jerusalem_2.jpg",
+            "images/Jerusalem/Jerusalem_3.jpg"
+        ]
+    },
+    {
+        name: "Lucerne, Switzerland",
+        lat: 47.0502,
+        lon: 8.3093,
+        preview: "images/Lucern/Lucern_1.jpg",
+        images: [
+            "images/Lucern/Lucern_1.jpg",
+            "images/Lucern/Lucern_2.jpg",
+            "images/Lucern/Lucern_3.jpg"
+        ]
+    },
+    {
+        name: "Madeira, Portugal",
+        lat: 32.7607,
+        lon: -16.9595,
+        preview: "images/Madeira/Madeira_1.jpg",
+        images: [
+            "images/Madeira/Madeira_1.jpg",
+            "images/Madeira/Madeira_2.jpg",
+            "images/Madeira/Madeira_3.jpg",
+            "images/Madeira/Madeira_4.jpg",
+            "images/Madeira/Madeira_5.jpg",
+            "images/Madeira/Madeira_6.jpg",
+            "images/Madeira/Madeira_7.jpg",
+            "images/Madeira/Madeira_8.jpg"
+        ]
+    },
+    {
+        name: "Mont Tremblant, Canada",
+        lat: 46.11667,
+        lon: -74.599998,
+        preview: "images/Mont_Tremblant/Mont_Tremblant_1.jpg",
+        images: [
+            "images/Mont_Tremblant/Mont_Tremblant_1.jpg",
+            "images/Mont_Tremblant/Mont_Tremblant_2.jpg",
+            "images/Mont_Tremblant/Mont_Tremblant_3.jpg"
+        ]
+    },
+    {
+        name: "Montreal, Canada",
+        lat: 45.5017,
+        lon: -73.5673,
+        preview: "images/Montreal/Montreal_1.jpg",
+        images: [
+            "images/Montreal/Montreal_1.jpg",
+            "images/Montreal/Montrel_2.jpg",
+            "images/Montreal/Montreal_3.jpg",
+            "images/Montreal/Montreal_4.jpg",
+            "images/Montreal/Montreal_5.jpg",
+            "images/Montreal/Montreal_6.jpg"
+        ]
+    },
+    {
+        name: "Ottawa, Canada",
+        lat: 45.4215,
+        lon: -75.6972,
+        preview: "images/Ottowa/Ottowa_1.jpg",
+        images: [
+            "images/Ottowa/Ottowa_1.jpg",
+            "images/Ottowa/Ottowa_2.jpg"
+        ]
+    },
+    {
+        name: "Porto, Portugal",
+        lat: 41.1579,
+        lon: -8.6291,
+        preview: "images/Porto/Porto_1.jpg",
+        images: [
+            "images/Porto/Porto_1.jpg",
+            "images/Porto/Porto_2.jpg",
+            "images/Porto/Porto_3.jpg",
+            "images/Porto/Porto_4.jpg",
+            "images/Porto/Porto_5.jpg",
+            "images/Porto/Porto_6.jpg"
+        ]
+    },
+    {
+        name: "Santorini, Greece",
+        lat: 36.3932,
+        lon: 25.4615,
+        preview: "images/Santorini/Santorini_1.jpg",
+        images: [
+            "images/Santorini/Santorini_1.jpg",
+            "images/Santorini/Santorini_2.jpg",
+            "images/Santorini/Santorini_3.jpg",
+            "images/Santorini/Santorini_4.jpg"
+        ]
     },
     {
         name: "Sydney, Australia",
         lat: -33.8688,
         lon: 151.2093,
-        preview: "https://picsum.photos/id/1047/200/150",
-        images: ["https://picsum.photos/id/1047/800/600", "https://picsum.photos/id/1050/800/600"]
+        preview: "images/Sydney/Sydney_1.jpg",
+        images: [
+            "images/Sydney/Sydney_1.jpg",
+            "images/Sydney/Sydney_2.jpg",
+            "images/Sydney/Sydney_3.jpg",
+            "images/Sydney/Sydney_4.jpg",
+            "images/Sydney/Sydney_5.jpg",
+            "images/Sydney/Sydney_6.jpg",
+            "images/Sydney/Sydney_7.jpg",
+            "images/Sydney/Sydney_8.jpg",
+            "images/Sydney/Sydney_9.jpg",
+            "images/Sydney/Sydney_10.jpg"
+        ]
     },
     {
-        name: "New York, USA",
-        lat: 40.7128,
-        lon: -74.0060,
-        preview: "https://picsum.photos/id/1067/200/150",
-        images: ["https://picsum.photos/id/1067/800/600", "https://picsum.photos/id/1076/800/600"]
+        name: "Toronto, Canada",
+        lat: 43.6532,
+        lon: -79.3832,
+        preview: "images/Toronto/Toronto_1.jpg",
+        images: [
+            "images/Toronto/Toronto_1.jpg",
+            "images/Toronto/Toronto_2.jpg",
+            "images/Toronto/Toronto_3.jpg"
+        ]
     }
 ];
 
